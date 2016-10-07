cfe7e757acf03d4d5f26a97d49e61b626d851ae2
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2016-10-06 23:38:57 -0700

    predict

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,12 +21,14 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
+a2 = sigmoid(X * Theta1');
+a2 = [ones(size(a2, 1), 1), a2];
+a3 = sigmoid(a2 * Theta2');
 
-
-
-
-
+[s, p] = max(a3, [], 2);
 
 
 % =========================================================================
